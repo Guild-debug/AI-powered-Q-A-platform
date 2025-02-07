00442c79faf010bcae22781ffe1ac58c05c6462c
Guild-debug <installersguild@gmail.com>
2025-02-07 15:44:25 +0300

    Update app.js

--- pages/app.js
+++ pages/app.js
@@ -1,9 +1,6 @@
-// pages/_app.js
 import '../styles/globals.css';
 import '../components/layout.css';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
